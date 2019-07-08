1413867dbac4158b9fa9f86813a0fd6c27128fa6
sidharth nayyar <sidnayyar1@gmail.com>
2019-07-07 22:35:38 -0400

    Calculate the result on second page.
    Note:Still an Error while running project

---
+++ project1/ResultViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ResultViewController.swift
+//  project1
+//
+//  Created by Sidharth Nayyar on 2019-07-07.
+//  Copyright © 2019 Sidharth Nayyar. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var lblMainResult: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- project1/ViewController.swift
+++ project1/ViewController.swift
@@ -10,11 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNumberText: UITextField!
+    @IBOutlet weak var secondNumberText: UITextField!
+    
+    @IBOutlet weak var lblResult: UILabel!
+    //    @IBOutlet weak var lblResult: UILabel!
+  //  @IBOutlet weak var textFieldInput: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
-
-
-}
+    @IBAction func btnMultiplication(_ sender: UIButton) {
+//        var a:Float
+//        var b:Float
+let num1 = Float(self.firstNumberText.text!)
+let num2 = Float(self.secondNumberText.text!)
+        self.lblResult.text = String(num1! * num2!)
+        
+    }
+    
+    @IBAction func btnDivision(_ sender: UIButton) {
+    
+        let num1 = Float(self.firstNumberText.text!)
+        let num2 = Float(self.secondNumberText.text!)
+        self.lblResult.text = String(num1! / num2!)
+    }
+    
+    @IBAction func btnAddition(_ sender: UIButton) {
+        let num1 = Float(self.firstNumberText.text!)
+        let num2 = Float(self.secondNumberText.text!)
+        self.lblResult.text = String(num1! + num2!)
+        
+        let SB = UIStoryboard(name: "Main", bundle: nil)
+        let result = SB.instantiateViewController(withIdentifier: "Result")as! ResultViewController
+        result.lblMainResult.text = self.lblResult.text
+        self.navigationController?.pushViewController(result, animated: true)
+    }
+    
+    @IBAction func btnSubtraction(_ sender: UIButton) {
+        let num1 = Float(self.firstNumberText.text!)
+        let num2 = Float(self.secondNumberText.text!)
+        self.lblResult.text = String(num1! - num2!)
+    
+    }
+    
+    
+    //    @IBAction func showResultbtn(_ sender: Any) {
+//        // suitable action we want to perform
+//       // self.textFieldInput.text = self.lblResult.text
+//        if let result = self.textFieldInput.text{
+//            self.lblResult.text = self.textFieldInput.text
+//        }
+//
+    }
 
